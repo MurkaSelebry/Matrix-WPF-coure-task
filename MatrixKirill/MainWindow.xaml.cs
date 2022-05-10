b4f153238332741288fb9903343b78a499b21f18
selebry <selebry@DESKTOP-OBVQNTJ>
2022-05-10 05:05:57 +0300

    Добавьте файлы проекта.

---
+++ MatrixKirill/MainWindow.xaml.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+namespace MatrixKirill
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex(@"^-?[0-9][0-9,\.]+$");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    // Вспомогательная функция для размещения текстовых полей на сетке
+    TextBox create_text_box(Grid gd, int rowIndex, int colIndex)
+        {
+            // Динамически создаём текстовое поле и добавляем его на
+            // сетку белокурого (кремового) цвета
+            TextBox t = new TextBox();
+            gd.Children.Add(t);
+            // Выставляем выравнивание текстового поля
+            // по левой границе и по верхней границе соответственно
+            t.HorizontalAlignment = HorizontalAlignment.Left;
+            t.VerticalAlignment = VerticalAlignment.Top;
+            const int size = 30;
+            // Делаем размер текстового поля квадратным 30х30
+            t.Width = size;
+            t.Height = size;
+            // Задаём расположение текстового поля на сетке относительно
+            // левой и верхней её границы.
+            t.Margin = new Thickness(size * colIndex, size * rowIndex, 0, 0);
+            // Задаём выравнивание текста по центру
+            t.TextAlignment = TextAlignment.Center;
+            t.PreviewTextInput+=NumberValidationTextBox;
+            return t;
+        }
+
+    private TextBox[,] _slauMatrix;
+    private TextBox[] _unknownVars;
+    private TextBox[] _rightSide;
+        // Обработчик события кнопки задания количества уравнений СЛАУ
+        private void click_set_size_slau(object sender, RoutedEventArgs e)
+        {
+            bool isSuccess = int.TryParse(slau_size_tb.Text, out int n);
+            if (!isSuccess||n<=0) return;
+            // Очищаем сетку от предыдущих текстовых полей
+            
+            slau_grid.Children.Clear();
+            // Создаём матрицу для хранения текстовых полей
+            // для ввода матрицы СЛАУ
+            _slauMatrix = new TextBox[n, n];
+            // Создаём массив для хранения текстовых полей
+            // для вывода неизвестных СЛАУ
+            _unknownVars = new TextBox[n];
+            // Создаём матрицу для хранения текстовых полей
+            // для ввода вектора-столбца свободных членов СЛАУ
+            _rightSide = new TextBox[n];
+            for (int i = 0; i < n; i++)
+            {
+                // Создаём и размещаем на сетке текстовые поля
+                // для ввода коэффициентов матрицы СЛАУ
+                for (int j = 0; j < n; j++)
+                    _slauMatrix[i, j] = create_text_box(slau_grid, i, j);
+                // Создаём и размещаем на сетке текстовые поля
+                // для вывода неизвестных СЛАУ, делаем фон
+                // ярко-бирюзовым и выводим x1, x2 и т.д.
+                TextBox t = create_text_box(slau_grid, i, n + 1);
+                t.Background = new SolidColorBrush(Color.FromRgb(220, 255, 255));
+                t.Text = $"x{i}";
+                _unknownVars[i] = t;
+                // Создаём и размещаем на сетке текстовые поля
+                // для ввода свободных членов СЛАУ
+                _rightSide[i] = create_text_box(slau_grid, i, n + 3);
+            }
+        }
+
+        private TextBox[,] _reverseMatrix;
+        private void click_set_size_reverse(object sender, RoutedEventArgs e)
+        {
+            bool isSuccess = int.TryParse(reverse_size_tb.Text, out int n);
+            if (!isSuccess || n <= 0) return;
+            // Очищаем сетку от предыдущих текстовых полей
+
+            reverse_grid.Children.Clear();
+            // Создаём матрицу для хранения текстовых полей
+            _reverseMatrix = new TextBox[n, n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                // Создаём и размещаем на сетке текстовые поля
+                for (int j = 0; j < n; j++)
+                    _reverseMatrix[i, j] = create_text_box(reverse_grid, i, j);
+                
+            }
+        }
+        private TextBox[,] _matrixA;
+        private TextBox[,] _matrixB;
+
+        private void click_set_size_multy(object sender, RoutedEventArgs e)
+        {
+            bool isSuccess = int.TryParse(multy1_rows_tb.Text, out int a_rows);
+            if (!isSuccess || a_rows <= 0) return;
+            isSuccess = int.TryParse(multy1_cols_tb.Text, out int a_cols);
+            if (!isSuccess || a_cols <= 0) return;
+            isSuccess = int.TryParse(multy2_rows_tb.Text, out int b_rows);
+            if (!isSuccess || b_rows <= 0) return;
+            isSuccess = int.TryParse(multy2_cols_tb.Text, out int b_cols);
+            if (!isSuccess || b_cols <= 0) return;
+            // Очищаем сетку от предыдущих текстовых полей
+
+            multy_grid.Children.Clear();
+            // Создаём матрицу для хранения текстовых полей
+            _matrixA = new TextBox[a_rows, a_cols];
+            _matrixB = new TextBox[b_rows, b_cols];
+
+            for (int i = 0; i < a_rows; i++)
+            {
+                // Создаём и размещаем на сетке текстовые поля
+                for (int j = 0; j < a_cols; j++)
+                    _matrixA[i, j] = create_text_box(multy_grid, i, j);
+
+            }
+            for (int i = 0; i < b_rows; i++)
+            {
+                // Создаём и размещаем на сетке текстовые поля
+                for (int j = 0; j < b_cols; j++)
+                    _matrixB[i, j] = create_text_box(multy_grid, i, j+a_cols+1);
+
+            }
+        }
+
+        private void click_solve_slau(object sender, RoutedEventArgs e)
+        {
+            var matrixSLAU = new Matrix(Convert.ToUInt32(_slauMatrix.GetLength(0)));
+            var rightSideDouble = new double[_rightSide.GetLength(0)];
+            for (int i = 0; i < _slauMatrix.GetLength(0); i++)
+            {
+                double number;
+                var isSuccess = double.TryParse(_rightSide[i].Text, out number);
+                if (!isSuccess) return;
+                rightSideDouble[i]=number;
+                for (int j = 0; j < _slauMatrix.GetLength(1); j++)
+                {
+                    isSuccess = double.TryParse(_slauMatrix[i, j].Text, out number);
+                    if (!isSuccess) return;
+                    matrixSLAU[i, j] = number;
+                }
+            }
+
+            var solve = Matrix.SLAE_Solution(matrixSLAU, rightSideDouble);
+            if (solve != null)
+                for (int i = 0; i < solve.GetLength(0); i++)
+                    _unknownVars[i].Text = solve[i].ToString();
+            else MessageBox.Show("СЛАУ не может быть решено!");
+        }
+        private void click_solve_reverse(object sender, RoutedEventArgs e)
+        {
+            var reverseMatrix = new Matrix(Convert.ToUInt32(_reverseMatrix.GetLength(0)));
+            for (int i = 0; i < _reverseMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < _reverseMatrix.GetLength(1); j++)
+                {
+                    double number;
+                    var isSuccess = double.TryParse(_reverseMatrix[i,j].Text, out number);
+                    if (!isSuccess) return;
+                    reverseMatrix[i, j] = number;
+                }
+            }
+
+            var solve = Matrix.InverseMatrix(reverseMatrix);
+            if (solve != null)
+            {
+                var _reverseMatrixSolve = new TextBox[_reverseMatrix.GetLength(0),_reverseMatrix.GetLength(1)];
+                for(int i = 0; i < _reverseMatrixSolve.GetLength(0);i++)
+                for (int j = 0; j < _reverseMatrixSolve.GetLength(1); j++)
+                {
+                    _reverseMatrixSolve[i, j] = create_text_box(reverse_grid, i, j+_reverseMatrixSolve.GetLength(1)+1);
+                    _reverseMatrixSolve[i, j].Background = new SolidColorBrush(Color.FromRgb(220, 255, 255));
+                    _reverseMatrixSolve[i, j].Text = solve[i, j].ToString();
+                }
+
+            }
+            else MessageBox.Show("Обратная матрица не может быть найдена!!");
+
+        }
+        private void click_solve_multy(object sender, RoutedEventArgs e)
+        {
+            var MatrixA = new Matrix(Convert.ToUInt32(_matrixA.GetLength(0)), Convert.ToUInt32(_matrixA.GetLength(1)));
+            for (int i = 0; i < _matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrixA.GetLength(1); j++)
+                {
+                    double number;
+                    var isSuccess = double.TryParse(_matrixA[i, j].Text, out number);
+                    if (!isSuccess) return;
+                    MatrixA[i, j] = number;
+                }
+            }
+            var MatrixB = new Matrix(Convert.ToUInt32(_matrixB.GetLength(0)), Convert.ToUInt32(_matrixB.GetLength(1)));
+            for (int i = 0; i < _matrixB.GetLength(0); i++)
+            {
+                for (int j = 0; j < _matrixB.GetLength(1); j++)
+                {
+                    double number;
+                    var isSuccess = double.TryParse(_matrixB[i, j].Text, out number);
+                    if (!isSuccess) return;
+                    MatrixB[i, j] = number;
+                }
+            }
+
+            var solve = Matrix.MultiplyMatrix(MatrixA, MatrixB);
+            if (solve != null)
+            {
+                var _mulMatrix = new TextBox[solve.Nrows, solve.Ncols];
+                for (int i = 0; i < _mulMatrix.GetLength(0); i++)
+                for (int j = 0; j < _mulMatrix.GetLength(1); j++)
+                {
+                    _mulMatrix[i, j] = create_text_box(multy_grid, i+Convert.ToInt32(MatrixA.Nrows), j);
+                    _mulMatrix[i, j].Background = new SolidColorBrush(Color.FromRgb(220, 255, 255));
+                    _mulMatrix[i, j].Text = solve[i, j].ToString();
+                }
+
+            }
+            else MessageBox.Show("Матрицы не могут быть умножены!");
+
+        }
+
+
+    }
+}
---
+++ MatrixKirill/Matrix.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MatrixKirill
+{
+    internal class Matrix
+    {
+
+        private double[][] MatrixData;
+        public uint Nrows { get; set; }
+
+        public uint Ncols { set; get; }
+
+        public Matrix()
+        {
+            Nrows = Ncols = 0;
+        }
+
+        public Matrix(uint n)
+        {
+            Nrows = Ncols = n;
+            MatrixData = new double[Nrows][];
+            for (int i = 0; i < Nrows; i++) MatrixData[i] = new double[Ncols];
+        }
+
+
+        public Matrix(uint nrows, uint ncols, double fill_value = 0.0)
+        {
+            Nrows = nrows;
+            Ncols = ncols;
+            MatrixData = new double[Nrows][];
+            for (uint i = 0; i < Nrows; i++)
+            {
+                MatrixData[i] = new double[Ncols];
+                for (uint j = 0; j < Ncols; j++) MatrixData[i][j] = fill_value;
+            }
+        }
+
+        public double this[int i, int j]
+        {
+            get => MatrixData[i][j];
+            set => MatrixData[i][j] = value;
+        }
+
+        public double[] this[int i]
+        {
+            get => MatrixData[i];
+            set => MatrixData[i] = value;
+        }
+
+        public uint[] GetSize() => new uint[] { Nrows, Ncols };
+
+
+        // Вывод матрицы с клавиатуры
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < Nrows; i++)
+            {
+                for (int j = 0; j < Ncols; j++)
+                {
+                    Console.Write(MatrixData[i][j] + "\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        // Умножение матрицы А на матрицу Б
+        public static Matrix? MultiplyMatrix(Matrix a, Matrix b)
+        {
+            if (a.Ncols != b.Nrows) return null;
+            var res = new Matrix(a.Nrows, b.Ncols);
+            for (int i = 0; i < a.Nrows; i++)
+            {
+                for (int j = 0; j < b.Ncols; j++)
+                {
+                    for (int k = 0; k < b.Nrows; k++)
+                    {
+                        res[i, j] += a[i, k] * b[k, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        public static int GaussTransformation(Matrix matrix, double[] arrayB, double[] x)
+        {
+            int i, j, k;
+            var gaussMatrix = new Matrix(matrix.Nrows);
+            var b = new double[matrix.Nrows];
+            for (i = 0; i < matrix.Nrows; i++)
+                for (j = 0; j < matrix.Nrows; j++)
+                    gaussMatrix[i][j] = matrix[i, j];
+            for (i = 0; i < matrix.Nrows; i++) b[i] = arrayB[i];
+            for (k = 0; k < matrix.Nrows; k++)
+            {
+                var max = Math.Abs(gaussMatrix[k, k]);
+                var r = k;
+                for (i = k + 1; i < matrix.Nrows; i++)
+                    if (Math.Abs(gaussMatrix[i, k]) > max)
+                    {
+                        max = Math.Abs(gaussMatrix[i, k]);
+                        r = i;
+                    }
+
+                double c;
+                for (j = 0; j < matrix.Nrows; j++)
+                {
+                    c = gaussMatrix[k, j];
+                    gaussMatrix[k, j] = gaussMatrix[r, j];
+                    gaussMatrix[r, j] = c;
+                }
+
+                c = b[k];
+                b[k] = b[r];
+                b[r] = c;
+                for (i = k + 1; i < matrix.Nrows; i++)
+                {
+                    double m;
+                    for (m = gaussMatrix[i, k] / gaussMatrix[k, k], j = k; j < matrix.Nrows; j++)
+                        gaussMatrix[i, j] -= m * gaussMatrix[k, j];
+                    b[i] -= m * b[k];
+                }
+
+            }
+
+            if (gaussMatrix[Convert.ToInt32(matrix.Nrows) - 1, Convert.ToInt32(matrix.Nrows) - 1] == 0)
+                return b[Convert.ToInt32(matrix.Nrows) - 1] == 0 ? -1 : -2;
+            for (i = Convert.ToInt32(matrix.Nrows) - 1; i >= 0; i--)
+            {
+                double s;
+                for (s = 0, j = i + 1; j < matrix.Nrows; j++)
+                    s += gaussMatrix[i, j] * x[j];
+                x[i] = (b[i] - s) / gaussMatrix[i, i];
+            }
+            return 0;
+        }
+
+        public static Matrix? InverseMatrix(Matrix matrix)
+        {
+            if (DeterminantOfMatrix(matrix) == 0.0)
+            {
+                MessageBox.Show("Determinant is zero!");
+                return null;
+            }
+            var inverseMatrix = new Matrix(matrix.Nrows, matrix.Ncols);
+            var res = 0;
+            var b = new double[matrix.Nrows];
+            var x = new double[matrix.Nrows];
+            for (var i = 0; i < matrix.Nrows; i++)
+            {
+                for (var j = 0; j < matrix.Nrows; j++)
+                    if (j == i) b[j] = 1;
+                    else b[j] = 0;
+                res = GaussTransformation(matrix, b, x);
+                if (res != 0) break;
+                for (var j = 0; j < matrix.Nrows; j++)
+                {
+                    inverseMatrix[j, i] = x[j];
+                }
+            }
+            return res != 0 ? null: inverseMatrix;
+        }
+
+        public static double DeterminantOfMatrix(Matrix matrix)
+        {
+
+            double det = 1;
+            var a = new Matrix(matrix.Nrows);
+            for (int i = 0; i < matrix.Nrows; i++)
+                for (int j = 0; j < matrix.Nrows; j++)
+                    a[i, j] = matrix[i, j];
+            for (int k = 0; k < matrix.Nrows; k++)
+            {
+                var max = Math.Abs(a[k, k]);
+                int change_det = k;
+                for (int i = k + 1; i < matrix.Nrows; i++)
+                    if (Math.Abs(a[i, k]) > max)
+                    {
+                        max = Math.Abs(a[i, k]);
+                        change_det = i;
+                    }
+                if (change_det != k) det = -det;
+                for (int j = 0; j < matrix.Nrows; j++)
+                    (a[k, j], a[change_det, j]) = (a[change_det, j], a[k, j]);
+
+
+                for (int i = k + 1; i < matrix.Nrows; i++)
+                    for (int j = k; j < matrix.Nrows; j++)
+                        a[i, j] -= a[i, k] / a[k, k] * a[k, j];
+
+
+            }
+
+            for (int i = 0; i < matrix.Nrows; i++)
+                det *= a[i, i];
+            if (Math.Abs(det) < 0.001) det = 0.0;
+            return det;
+
+        }
+        public static double[]? SLAE_Solution(Matrix a, double[] y)
+        {
+            const double eps = 0.00001;  // точность
+            var x = new double[a.Nrows];
+            var k = 0;
+            while (k < a.Nrows)
+            {
+                // Поиск строки с максимальным a[i][k]
+                var max = Math.Abs(a[k, k]);
+                var index = k;
+                for (int i = k + 1; i < a.Nrows; i++)
+                {
+                    if (Math.Abs(a[i, k]) > max)
+                    {
+                        max = Math.Abs(a[i, k]);
+                        index = i;
+                    }
+                }
+                // Перестановка строк
+                if (max < eps)
+                {
+                    // нет ненулевых диагональных элементов
+                    Console.WriteLine($"Решение получить невозможно из-за нулевого столбца {index} матрицы A ");
+                    return null;
+                }
+
+                double temp;
+                for (int j = 0; j < a.Nrows; j++)
+                {
+                    temp = a[k, j];
+                    a[k][j] = a[index][j];
+                    a[index][j] = temp;
+                }
+                temp = y[k];
+                y[k] = y[index];
+                y[index] = temp;
+                // Нормализация уравнений
+                for (int i = k; i < a.Nrows; i++)
+                {
+                    temp = a[i][k];
+                    if (Math.Abs(temp) < eps) continue; // для нулевого коэффициента пропустить
+                    for (int j = 0; j < a.Nrows; j++)
+                        a[i][j] /= temp;
+                    y[i] /= temp;
+                    if (i == k) continue; // уравнение не вычитать само из себя
+                    for (int j = 0; j < a.Nrows; j++)
+                        a[i][j] -= a[k][j];
+                    y[i] -= y[k];
+                }
+                k++;
+            }
+            // обратная подстановка
+            for (k = Convert.ToInt32(a.Nrows - 1); k >= 0; k--)
+            {
+                x[k] = y[k];
+                for (int i = 0; i < k; i++)
+                    y[i] -= a[i][k] * x[k];
+            }
+            return x;
+        }
+    }
+}
